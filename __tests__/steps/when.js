ab24700080305bd12579cddfc0ca963446a574ae
o/sarfraaz <oslash@Sarfraazs-MacBook-Air.local>
2021-01-16 14:46:13 +0530

    GetMyProfile Resolver with dynamoDb source added

--- __tests__/steps/when.js
+++ __tests__/steps/when.js
@@ -1,5 +1,8 @@
 require("dotenv").config();
 const AWS = require("aws-sdk");
+const fs = require("fs");
+const velocityMapper = require("amplify-appsync-simulator/lib/velocity/value-mapper/mapper");
+const velocityTemplate = require("amplify-velocity-template");
 
 const we_invoke_confirm_user_signup = async (username, name, email) => {
   const handler = require("./../../functions/confirm-user-signup").handler;
@@ -64,7 +67,18 @@ const a_user_signs_up = async (password, name, email) => {
   };
 };
 
+const we_invoke_appsync_template = (templatePath, context) => {
+  const template = fs.readFileSync(templatePath, { encoding: "utf-8" });
+  const ast = velocityTemplate.parse(template);
+  const compiler = new velocityTemplate.Compile(ast, {
+    valueMapper: velocityMapper.map,
+    escape: false,
+  });
+  return JSON.parse(compiler.render(context));
+};
+
 module.exports = {
   we_invoke_confirm_user_signup,
   a_user_signs_up,
+  we_invoke_appsync_template,
 };
